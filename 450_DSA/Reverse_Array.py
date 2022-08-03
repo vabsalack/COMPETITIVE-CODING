a2907c8eb6a706e236045f823defbc3974f89e86
keerthivasan <keerthi.pydev@gmail.com>
2022-08-03 19:30:06 +0530

    450 DSA array 01

---
+++ 450_DSA/Reverse_Array.py
@@ -0,0 +1,19 @@
+""" https://practice.geeksforgeeks.org/problems/reverse-an-array/0 """
+
+
+def main(A, N):
+    limit = N // 2
+    for i in range(0, limit):
+        A[i] = A[i] + A[N - 1 - i]
+        A[N - 1 - i] = A[i] - A[N - 1 - i]
+        A[i] = A[i] - A[N - 1 - i]
+
+    print(*A)
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        A = list(map(int, input().split()))
+        main(A, N)
