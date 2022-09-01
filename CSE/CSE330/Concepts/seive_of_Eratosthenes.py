4a1dee7aec38f6cf73d657d9390491a3937957a1
keerthivasan <keerthi.pydev@gmail.com>
2022-09-01 22:04:32 +0530

    sieve of eratosthenes optimization

--- CSE/CSE330/Concepts/seive_of_Eratosthenes.py
+++ CSE/CSE330/Concepts/seive_of_Eratosthenes.py
@@ -1,43 +1,36 @@
-import math
+from math import *
 
 
-def check_prime(x):
+def primality(x):
+    if x <= 1:
+        return False
     if x == 2 or x == 3:
         return True
     if x % 2 == 0 or x % 3 == 0:
         return False
-    for i in range(5, math.floor(math.sqrt(x))+1, 6):
+    for i in range(5, floor(sqrt(x))+1, 6):
         if x % i == 0 or x % (i + 2) == 0:
             return False
     return True
 
 
-def naive_method(n):
-    """ to find all prime number up to n
-        time complexity for this one is O(n*)"""
-    result = []
-    for i in range(2, n+1):
-        if check_prime(i):
-            result.append(i)
-    return result
+def sieve_of_Eratosthenes(n):
+    sieve = [1] * (n + 1)
+    sieve[0] = sieve[1] = 0
+    i = 2
+    while i * i <= n:
+        if sieve[i] == 1:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = 0
+        i += 1
 
-
-def sieve_of_eratosthenes(n):
-    """ by using sieve of eratosthenes the time complexity is o(nlog(log(n)))"""
-    p = [1]*(n+1)
-    for i in range(2, n+1):
-        if p[i] == 1:
-            for j in range(i+i, n+1, i):
-                p[j] = 0
-
-    for i in range(2, len(p)):
-        if p[i] == 1:
+    for i in range(n):
+        if sieve[i]:
             print(i, end=" ")
 
 
 if __name__ == "__main__":
-    x = int(input("> "))
-    res = naive_method(x)
-    print(*res)
-    sieve_of_eratosthenes(x)
+    num = int(input())
+    sieve_of_Eratosthenes(num)
+
 
