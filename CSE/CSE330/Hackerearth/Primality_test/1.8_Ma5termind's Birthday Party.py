791147ae62ae421b2a4c469e9562dab2c4c216e8
keerthivasan <keerthi.pydev@gmail.com>
2022-09-02 10:39:38 +0530

    master mind birthday party code optimized

--- CSE/CSE330/Hackerearth/Primality_test/1.8_Ma5termind's Birthday Party.py
+++ CSE/CSE330/Hackerearth/Primality_test/1.8_Ma5termind's Birthday Party.py
@@ -1,28 +1,28 @@
 """https://www.hackerearth.com/practice/math/number-theory/primality-tests/practice-problems/algorithm/ma5terminds-birthday-party/"""
 
 
-def erathonos(n):
+def sieve_count(n):
     sieve = [1] * (n + 1)
+    sieve[0] = 0
     sieve[1] = 1
-
     for i in range(2, n + 1):
         if sieve[i] == 1:
-            for j in range(i * i, n, i):
-                if sieve[j] == 1:
-                    sieve[j] = 0
+            for j in range(i * i, n + 1, i):
+                sieve[j] = 0
+    s = 0
+    for i in range(n + 1):
+        s += sieve[i]
+        sieve[i] = s
+
     return sieve
 
 
-sieve = erathonos(1000000)
+sieve = sieve_count(1000000)
 
 
 def main(l, r):
-    count = 0
-    for i in range(l, r + 1):
-        if not sieve[i]:
-            count += 1
-
-    return count
+    remain = sieve[r] - sieve[l - 1]
+    return r - l + 1 - remain
 
 
 if __name__ == "__main__":
