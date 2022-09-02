982317821239d64c3efe4e5e6dded21eebeef444
keerthivasan <keerthi.pydev@gmail.com>
2022-09-02 11:06:07 +0530

    micro prime prime optimized

--- CSE/CSE330/Hackerearth/Primality_test/1.5_Micro and Prime Prime.py
+++ CSE/CSE330/Hackerearth/Primality_test/1.5_Micro and Prime Prime.py
@@ -1,65 +1,26 @@
-from math import *
+def sieve_count(x):
+    sieve1 = [1]*(x+1)
+    sieve1[0] = sieve1[1] = 0
+    for i in range(2, x+1):
+        if sieve1[i] == 1:
+            for j in range(i*i, x+1, i):
+                sieve1[j] = 0
+    s = 0
+    sie_count1 = [0]*(x+1)
+    for i in range(x+1):
+        s += sieve1[i]
+        sie_count1[i] = s
+    return sieve1, sie_count1
 
 
-def sieveofatkin(limit):
-    count = 0
-    if limit >= 2:
-        count += 1
-    if limit >= 3:
-        count += 1
-    sieve = [False] * (limit + 1)
-    x = 1
-    while x * x <= limit:
-        y = 1
-        while y * y <= limit:
-            n = 4 * x * x + y * y
-            if n <= limit and (n % 12) in [1, 5]:
-                sieve[n] ^= True
-            n = 3 * x * x + y * y
-            if n <= limit and n % 12 in [7]:
-                sieve[n] ^= True
-            n = 3 * x * x - y * y
-            if x > y and n <= limit and n % 12 in [11]:
-                sieve[n] ^= True
-            y += 1
-        x += 1
-
-    r = 5
-    while r * r <= limit:
-        if sieve[r]:
-            for i in range(r * r, limit + 1, r * r):
-                sieve[i] = False
-        r += 1
-
-    for i in range(5, limit + 1):
-        if sieve[i]:
-            count += 1
-    return count
-
-
-def primality(x):
-    if x <= 1:
-        return False
-    if x == 2 or x == 3:
-        return True
-    if x % 2 == 0 or x % 3 == 0:
-        return False
-    for i in range(5, floor(sqrt(x)) + 1, 6):
-        if x % i == 0 or x % (i + 2) == 0:
-            return False
-    return True
+sieve, sie_count = sieve_count(1000000)
 
 
 def main(l, r):
-    memo = sieveofatkin(l-1)
     count = 0
-
     for i in range(l, r + 1):
-        if primality(i):
-            memo += 1
-        if primality(memo):
+        if sieve[sie_count[i]]:
             count += 1
-
     return count
 
 
