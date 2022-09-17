84247e6feade04d51e202172f4e94fb756e16edd
keerthivasan <keerthi.pydev@gmail.com>
2022-09-17 09:28:42 +0530

    top view tree solved

---
+++ CSE/CSE331/week2_Tree/2.1_top_view_tree.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+
+
+def topView(root):
+    # Write your code here
+    heights = {}
+
+    def recur(root, h, l):
+
+        if root:
+
+            if h not in heights:
+                heights[h] = [root, l]
+
+            elif heights[h][1] > l:
+                heights[h] = [root, l]
+
+            recur(root.left, h - 1, l + 1)
+            recur(root.right, h + 1, l + 1)
+
+    recur(root, 0, 0)
+
+    temp = sorted(heights)
+
+    for i in temp:
+        print(heights[i][0].info, end=" ")
+
+
