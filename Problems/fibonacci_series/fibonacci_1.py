4f4d86f88351476fc9d39533ee2d34f4649dbca3
keerthivasan <keerthi.pydev@gmail.com>
2022-10-10 22:41:57 +0530

    fib redone

--- Problems/fibonacci_series/fibonacci_1.py
+++ Problems/fibonacci_series/fibonacci_1.py
@@ -1,16 +1,20 @@
-memo = [-1] * 100
+def fib(n):
+    """0, 1
+       1 1 2 3 5 8 13 21 ..."""
+    if n in memo:
+        return memo[n]
 
+    if n < 2:
+        return 1
 
-def fibonacci(i):
-    if memo[i] != -1:
-        return memo[i]
-    if i <= 1:
-        return i
-    memo[i] = fibonacci(i - 1) + fibonacci(i - 2)
-    return memo[i]
+    memo[n] = fib(n - 1) + fib(n - 2)
+    return memo[n]
 
 
 if __name__ == "__main__":
-    index = int(input())
-    re = fibonacci(index)
-    print(re)
+    memo = {}
+    print(fib(int(input())))
+    print(memo)
+
+
+
